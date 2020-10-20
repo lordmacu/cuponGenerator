7d0785a1296f62193e516f82521c08ae69f7d4f8
Crsitian Camilo Garcia <informacion@cristiangarcia.co>
2020-10-20 10:00:36 -0500

    Create generator.sh

---
+++ generator.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+if [ -z $1 ]; then
+    echo "Pass file size as initial parameter"
+    exit
+fi
+
+SIZE=$1
+items=$2
+
+awk -v size="$SIZE" -v items="$items" '
+
+BEGIN{ 
+	for (o=1; o<=items; o++) {
+	    srand(o);  
+	    NUM=size;  
+	    idx=0;  
+
+	    #add_range(97,122)   # letters
+
+	    idx=65;
+	    while (idx <= 90) {
+	        alphabet[idx] = sprintf("%c",idx)
+	        idx+=1
+	    }
+
+	    idx=0
+	    for (k in alphabet){
+	        alphanums[idx]=alphabet[k]
+	        idx+=1
+	    }
+	    alphabet_len = idx
+	    i=0
+
+	    # and iterate to random some characters
+	    idx =0
+	            printf("CO57")
+
+	    while (idx < NUM){                         
+	        dec =0
+	        char_idx=int(rand() * alphabet_len)
+	        char = alphanums[char_idx]
+	        printf("%s",alphanums[char_idx])
+	        idx+=1
+	    } 
+
+	    print("") 
+    }
+}  
+' 
